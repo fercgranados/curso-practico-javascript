3b4e3b550280e3e88c0321f9938390a4351fe7d5
rcgranados <fercgranados@gmail.com>
2022-01-27 22:31:12 -0600

    Funciones para figuras geometricas parte2

--- funciones.js
+++ funciones.js
@@ -7,5 +7,32 @@ function perimemetroCuadrado (lado){
 function areaCuadrado (lado){
     return lado*lado;
 }
+console.groupEnd();
+
+
+
+console.group("Triángulo");
+
+function perimetroTriangulo (lado1, lado2, base){
+    return lado1+lado2+base;
+}
+function areaTriangulo (base, altura){
+    return (base*altura)/2;
+}
+console.groupEnd();
+
+console.group("Círculo");
+
+function diametroCirculo(radio){
+    return radio*2;
+}
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro*Math.PI;
+}
+
+function areaCirculo(radio){
+    return Math.PI*(radio*radio);
+}
 
 console.groupEnd();
